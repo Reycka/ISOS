94fba7e8f129f2ad550618e5f41c69e68735c358
Reycka <157039413+Reycka@users.noreply.github.com>
2024-10-24 00:43:40 +0200

    Actualización en SlotClass y creacion de la clase BattleManager y Matriz

---
+++ Chamber Of The Heart/Scripts/Combate/BattleManager.js
@@ -0,0 +1,35 @@
+import SlotClass from SlotClass
+export default class BattleManager extends Phaser.GameObject.Sprite{
+   
+};
+export class Matriz{
+    mat; //Matriz de enteros que representa la posición donde se colocará la tropa
+    matBool; //Matriz que indca si la casilla está ocupada o no
+    row; //Filas de la matriz
+    col; //Columnas de la matriz
+    constructor(_fil, _col){
+        //Construye la dos matrices, la que almacena las tropas y la que indica si hay tropa o no
+        //En este caso siempre las inicializa vacías, es decir, a False
+        this.fil = _fil;
+        this.col = _col;
+        this.mat = new Array[this.row,this.col];
+        for(let i = 0; i < this.row; i++){
+            for(let j = 0; j < this.col; j++){
+                mat[i][j] = new SlotClass();
+            }
+        }
+    }
+    render(){
+        for(let i = 0; i < this.row; i++){
+            for(let j = 0; j < this.col; i++){
+                console.log("Me pinto" + "\n");
+                deph.number(0); //Colocamos al fondo todo
+                this.add.image('backgroundMat',"IP"); //Colocamos el fondo
+                if(mat[i,j].GetState()){ //Comprobamos si la matriz está ocupada
+                    console.log("Estoy ocupada" + "\n"); //Debug
+                    mat[i,j].render(); //Si lo está renderizamos la tropa
+                }
+            }
+        }
+    }
+}
--- Chamber Of The Heart/Scripts/Combate/SlotClass.js
+++ Chamber Of The Heart/Scripts/Combate/SlotClass.js
@@ -1,77 +1,40 @@
 //import Unit from UnitClass cuando tengamos la clase Unit hecha
-export default class SlotClass extends Phaser.sprites{
+export default class SlotClass extends Phaser.GameObject.sprites{
 ///PROPIEDADES
- unit
- //Tamaño de la matriz
- row = 0;
- col = 0;
- //Matrices a usar, una representa la matriz como tal para ser rellenada con tropas mientra que la otra se encarga de controlar si está ocupada la casilla o no
- mat;
- matBool;
- //Asigna el tamaño a la matriz y setea todo a False en un principio
-
+ unit;
+ ocupada;
 //CONSTRUCTOR
-//REVISAR EL COMO PASO LA TROPA
-    constructor(_fil, _col, _unit){
+    constructor(){
         console.log("Me construyo" + "\n");
-        this.row = _fil;
-        this.col = _col;
-        this.unit = _unit;
-        this.mat = new Array[this.row,this.col];
-        this.matBool = this.mat;
-        for(let i = 0; i < this.row; i++){
-            for(let j = 0; j < this.col; j++){
-                this.mat[i,j] = this.unit;
-                if(mat[i,j] == null || mat[i,j] == undefined){
-                    this.matBool[i,j] = false;
-                }
-                else this.matBool[i,j] = true;
-            }
-        }
+        this.unit = null;
+        ocupada = false;
     }
-//MÉTODOS
     render(){
-        for(let i = 0; i < this.row; i++){
-            for(let j = 0; j < this.col; i++){
-                console.log("Me pinto" + "\n");
-                deph.number(0);
-                //Colocamos la Capa en la posición del fondo
-                //Comando para colocar la imagen
-                if(this.GetState(i,j)){
-                    console.log("Estoy ocupada" + "\n"); //Debug
-                    deph.number(1);
-                    //Colocanos la Capa en posición tropa
-                    //Comando para colocar la imagen
-                }
-            }
-        }
+        deph.number(1); //Colocamos la imagen por encima
+        this.add.sprites('ID', "IP"); //Seteamos la imagen
     }
     //coloca la unidad en la casilla y pone el booleano ocupada en true.
-    //Unit Representa la Unidad a colocar
-    //posX y posY representan la casilla en la que se quiere colocar
     SetUnit(unit,posX,posY){
-        if(!this.GetState(posX,posY)){
+        if(!this.GetState()){
             console.log("Seteo" + "\n"); //Debug
-            mat[posX,posY] = unit; //Cuando se tenga la clase Unit ver como se hace
-            this.matBool[posX,posY] = true;
+            this.render();
+            ocupada = true;
         }
     }
     //Devuelve si hay una tropa o no en dicha posición
-    GetState(posX,posY){
-        console.log(this.matBool[posX,posY] + "\n");
-        return this.matBool[posX,posY];
+    GetState(){
+        return this.ocupada;
     }   
-    //Método que se llamará cada vez que una tropa muera o se desplace, setea a false el si hay tropa y vacía la posición en la matriz
-    SetFree(posX,posY){
-            this.matBool[psoX,posY] = false;
+    //Método que se llamará cada vez que una tropa muera o se desplace, coloca a False su valor ocupada
+    SetFree(){
+           this.ocupada = false;
     }
-    
     //Asigna la tropa pasada a la nueva posición y setea a true en la matriz de booleanos
-    SetFull(posX,posY){
-        if(!this.GetState(posX,posY)){
+    SetFull(mat){ //mat representa la posición nueva a seteear, es un slotClass que hay que pasarle
+        if(mat.GetState()){
             //Llamada al método que cambia la posición de la tropa
-            this.mat[posX,posY] = null; //EN LUGAR DE NULL ASIGNAR LA TROPA PASADA
-            this.matBool[posX,posY] = true;
+           this.ocupada = false;
+           mat.ocupada = true;
         }
     }
 

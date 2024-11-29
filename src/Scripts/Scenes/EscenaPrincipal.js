d8366b7bf642c3ab4c2ecf22f4629d7e0c9ada3e
Reycka <157039413+Reycka@users.noreply.github.com>
2024-11-29 18:57:50 +0100

    Menu Inicial
    Faltan que las letras sean más estéticas, no las por defecto del css

--- src/Scripts/Scenes/EscenaPrincipal.js
+++ src/Scripts/Scenes/EscenaPrincipal.js
@@ -17,23 +17,27 @@ export default class EscenaPrincipal extends Phaser.Scene {
 	preload() {
 
 		this.load.image('Background','src/Assets/Finales/fondo_socializar.png')
-
-		this.load.image('BotonPrueba', 'src/Assets/Temporales/PlaceHolderCat.png');
-		
+		this.load.audio('fondito','src/Assets/sfx/musica/FINALES/Ethereal Heartbeat Main.WAV')
 	}
 	
 	create() {
-		var back= this.add.image(this.sys.game.canvas.width / 2, this.sys.game.canvas.height / 2,'Background');
-		
-		//back.setScale(this.cameras.main.width / this.textures.get('Background').getSourceImage().width,
-        //this.cameras.main.height / this.textures.get('Background').getSourceImage().height);
-		var sprite = this.add.image(this.sys.game.canvas.width / 2, this.sys.game.canvas.height*3 / 4, 'BotonPrueba')
-		sprite.setInteractive(); 
+		var audio = this.sound.add('fondito')
+		audio.play({loop:true});
 		var inventory = new Inventory();
 		inventory.AddGift(6);
-		//levantando el click izquierdo
-		sprite.on('pointerup', pointer => {
+		this.add.image(this.sys.game.canvas.width / 2, this.sys.game.canvas.height / 2,'Background');
+		var start = this.add.text(800,500,"EMPEZAR");
+		start.setScale(5,5);
+		var exit = this.add.text(850,600,"SALIR");
+		exit.setScale(5,5);
+		start.setInteractive(); 
+		exit.setInteractive();
+		start.on('pointerup', pointer => {
+			audio.stop();
 			this.scene.start('EscenaSocialTienda',inventory);
+		})
+		exit.on('pointerup', pointer => {
+			location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 		})
 	}
 
